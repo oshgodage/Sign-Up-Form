17e5ae54eb5707de74bb29f35bcb5bb0cc793f2b
oshadiVindya <vindyagodage@gmail.com>
2020-10-24 12:07:47 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {useState, useEffect }from 'react';
+
 import './App.css';
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className ="container">
+      <form id="Form1"> 
+        <h3>CREATE ACCOUNT</h3>
+        <input type="text" placeholder="Email" required></input>
+        <input type="password" placeholder="Password" required></input>
+        <input type="password" placeholder="Confirm Password" required></input>
+
+        <div class="btn-box">
+          <button type="button" id="Next1" >Next</button>
+        </div>
+      </form>
+
+      <form id="Form2">
+        <h3>SOCIAL LINKS</h3>
+        <input type="text" placeholder="Medium" required></input>
+        <input type="text" placeholder="Github" required></input>
+        <input type="text" placeholder="Linkedin" required></input>
+        <div class="btn-box">
+          <button type="button" id="Back1" >Back</button>
+          <button type="button" id="Next2" >Next</button>
+        </div>
+      </form>
+
+      <form id="Form3">
+        <h3>PERSONAL INFO</h3>
+        <input type="text" placeholder="First Name" required></input>
+        <input type="text" placeholder="Last Name" required></input>
+        <input type="text" placeholder="Mobile No" required></input>
+        <div class="btn-box">
+          <button type="button" id="Back2" >Back</button>
+          <button type="Submit" id="Submit" >Submit</button>
+        </div>
+      </form>
+
+      <div class="step-row">
+        <div id="progress"></div>
+        <div class="step-col"><small>Step 1</small></div>
+        <div class="step-col"><small>Step 2</small></div>
+        <div class="step-col"><small>Step 3</small></div>
+        </div>
+        
     </div>
+      
   );
 }
 
